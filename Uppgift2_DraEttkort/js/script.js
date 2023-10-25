9646842429c7a5ecfa4189a3f05e57595d740e1a
Samuel Lidström <samuel.lidstrom@gmail.com>
2023-10-25 17:23:28 +0200

    added  createElement, setAttribute & appendChild

--- Uppgift2_DraEttkort/js/script.js
+++ Uppgift2_DraEttkort/js/script.js
@@ -1,35 +1,10 @@
 const button = document.querySelector('#button');
-// const form = document.querySelector('#inputForm');
-// let formField = document.querySelector('#formField');
 const showCard = document.querySelector('#showCard');
 let fullUri = 'https://deckofcardsapi.com/api/deck/new/draw/?count=1';
-
-// inputField.addEventListener('change', function(){
-//     inputField = this.value;
-//     console.log(this.value);
-//     fullUri = `https://www.swapi.tech/api/people/?name=${this.value}`;
-//     console.log(fullUri);
-// })
-
-// form['formField'].addEventListener('change', function () {
-//     formField = this.value;
-//     console.log(this.value);
-//     fullUri = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`;
-//     console.log(fullUri);
-// })
-
-
-
-
-// form['formButton'].addEventListener('click', getApi);
 
 button.addEventListener('click', getApi);
 
 function getApi() {
-    /*Skriv din kod här*/
-    // textArea.innerHTML = ('...Computing...')
-
-
     fetch(fullUri)
         .then(res => res.json())
         .then(data => {
@@ -39,9 +14,13 @@ function getApi() {
             const cardImage = dataArray[0].image;
 
             console.log(cardImage);
-            showCard.innerHTML = (`<img src="${cardImage}" alt="">`)
 
-            
+            let drawnCard = document.createElement('drawnCard')
+            drawnCard.id = 'drawnCard';
+            drawnCard.innerHTML = `<img src="${cardImage}" alt="">`
+
+            showCard.innerHTML = ('');
+            showCard.appendChild(drawnCard);
 
         })
         .catch(err => console.log(err))
